299916441ded994881f6e10ff1f20119d23267da
ekoechmboya <ekoech.mboya@gmail.com>
2023-03-05 16:14:21 +0300

    created a shoeapp using django web framework

---
+++ base/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('create-shoe/', views.createShoe, name="create-shoe"),
+    path('delete-shoe/<str:pk>/', views.deleteShoe, name="delete-shoe"),
+    path('update-shoe/<str:pk>/', views.updateShoe, name="update-shoe")
+]
---
+++ base/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from .models import Shoe
+from .forms import RoomForm
+
+
+# Create your views here.
+
+
+def home(request):
+    shoes = Shoe.objects.all()
+    context = {'shoes': shoes}
+    return render(request, 'home.html', context)
+
+
+def createShoe(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'shoeform.html', context)
+
+
+def updateShoe(request, pk):
+    shoe = Shoe.objects.get(id=pk)
+    form = RoomForm(instance=shoe)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=shoe)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    context = {'form': form}
+    return render(request, 'shoeform.html', context)
+
+
+def deleteShoe(request, pk):
+    shoe = Shoe.objects.get(id=pk)
+    if request.method == 'POST':
+        shoe.delete()
+        return redirect('home')
+    return render(request, 'delete.html', {'obj': shoe})
